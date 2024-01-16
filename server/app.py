6dc8604c88f932fc6739601ba6690eef55f07efd
karenhernandez <khernandez0329@gmail.com>
2024-01-15 16:34:51 -0800

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,27 +1,64 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    body = {"message": "Flask SQLAlchemy Lab 1"}
     return make_response(body, 200)
 
+
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquakes(id):
+    earthquake = db.session.get(Earthquake, id)
+
+    if earthquake:
+        return (
+            jsonify(
+                {
+                    "id": earthquake.id,
+                    "location": earthquake.location,
+                    "magnitude": earthquake.magnitude,
+                    "year": earthquake.year,
+                }
+            ),
+            200,
+        )
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 
-if __name__ == '__main__':
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquakes_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quake_list = []
+    for earthquake in earthquakes:
+        quake_list.append(
+            {
+                "id": earthquake.id,
+                "location": earthquake.location,
+                "magnitude": earthquake.magnitude,
+                "year": earthquake.year,
+            }
+        )
+
+    return jsonify({"count": len(quake_list), "quakes": quake_list}), 200
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
